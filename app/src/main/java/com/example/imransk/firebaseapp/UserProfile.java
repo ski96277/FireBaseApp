4f1d895d65a66512696a84c5f6a633ed521c181f
imran sk <imran.bu96@gmail.com>
2018-03-23 10:07:46 +0600

    get value From data base

--- app/src/main/java/com/example/imransk/firebaseapp/UserProfile.java
+++ app/src/main/java/com/example/imransk/firebaseapp/UserProfile.java
@@ -7,7 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by imran sk on 3/20/2018.
@@ -23,11 +31,31 @@ public class UserProfile extends Fragment{
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+       final DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
+
+        final EditText user_name=view.findViewById(R.id.name_ET);
         Button profile=view.findViewById(R.id.profilebutton);
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getContext(), "hi i'm user profile", Toast.LENGTH_SHORT).show();
+
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+//get value From data base
+                        String name=dataSnapshot.child("UserInformation").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("name").getValue(String.class);
+                        user_name.setText(name);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
             }
         });
     }

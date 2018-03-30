1f51cb42ff83f6bf2caa75b27111b4fd040694da
imran sk <imran.bu96@gmail.com>
2018-03-30 23:21:02 +0600

    upload image to storage

--- app/src/main/java/com/example/imransk/firebaseapp/UpdateProfileF.java
+++ app/src/main/java/com/example/imransk/firebaseapp/UpdateProfileF.java
@@ -1,6 +1,12 @@
 package com.example.imransk.firebaseapp;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,12 +14,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by imran sk on 3/20/2018.
@@ -24,11 +41,33 @@ public class UpdateProfileF extends Fragment {
     Button updateBtn;
     FirebaseUser firebaseUser;
     FirebaseDatabase firebaseDatabase;
+    private StorageReference storageReference;
+
+    ImageView imageView;
+    Uri filePath;
+    String userId="";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.updateprofilef, null);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 200 && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap;
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     @Override
@@ -39,9 +78,23 @@ public class UpdateProfileF extends Fragment {
         age_ET = view.findViewById(R.id.ageET);
         updateBtn = view.findViewById(R.id.update_btn);
 
+        imageView = view.findViewById(R.id.image_view);
+//get the dfirebase storage refarance
+        storageReference = FirebaseStorage.getInstance().getReference();
+
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        final String userId=firebaseUser.getUid();
-        Toast.makeText(getContext(), ""+userId, Toast.LENGTH_SHORT).show();
+       userId = firebaseUser.getUid();
+        Toast.makeText(getContext(), "" + userId, Toast.LENGTH_SHORT).show();
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "select image"), 200);
+            }
+        });
+
         updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,15 +103,44 @@ public class UpdateProfileF extends Fragment {
                 String number = number_ET.getText().toString();
                 String age = age_ET.getText().toString();
                 UserInformation userInformation = new UserInformation(name, number, age);
-
+                uplload_Image();
 
                 firebaseDatabase = FirebaseDatabase.getInstance();
-                DatabaseReference databaseReference=firebaseDatabase.getReference("UserInformation");
+                DatabaseReference databaseReference = firebaseDatabase.getReference("UserInformation");
 //                databaseReference.child(userId);
                 databaseReference.child(userId).setValue(userInformation);
 
                 Toast.makeText(getContext(), "You click update button", Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
+
+    private void uplload_Image() {
+        storageReference=storageReference.child(userId).child("image/profile.jpg");
+
+        storageReference.putFile(filePath)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(getContext(), "Uploaded.......",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+            }
+        });
+
+
+    }
+
+
 }
